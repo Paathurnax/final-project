174db90666188c24ca6b20d877114f7ba258b91c
Jacob <165198998+Paathurnax@users.noreply.github.com>
2024-11-18 14:12:44 -0600

    craeted final project repo and template

---
+++ sketch.js
@@ -0,0 +1,15 @@
+// final thingy
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
