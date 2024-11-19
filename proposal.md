43248648e8b9d3fec683a1b4b16e0f1b65fb362c
Jacob <165198998+Paathurnax@users.noreply.github.com>
2024-11-19 14:16:59 -0600

    added asteroid object and creaetd proposal.md

---
+++ proposal.md
@@ -0,0 +1,12 @@
+# Project Description
+- i plan to make a program that lets the user pick between multiple games. Kind of like a very small scale cool math games.
+
+## Needs To Have
+- at least 3 different games
+- good looking ui
+- easy to play games
+- 2d and 3d games
+
+## Nice To Have
+- multiplayer games
+- 6 or more games
--- sketch.js
+++ sketch.js
@@ -1,10 +1,15 @@
 // Game Player
 // Jacob Koshman
-// January 31st, 2024
+// November 19th, 2024
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let asteroid;
+
+function preload() {
+  asteroid = loadModel("asteroid.obj");
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -12,4 +17,5 @@ function setup() {
 
 function draw() {
   background(220);
+  orbitControl();
 }
