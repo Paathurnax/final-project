4d03b5a9de4e574658152c6bca965f51197238e4
Jacob <165198998+Paathurnax@users.noreply.github.com>
2024-11-20 21:28:09 -0600

    made some inconsequential edits

--- sketch.js
+++ sketch.js
@@ -6,21 +6,42 @@
 // - describe what you did to take this project "above and beyond"
 
 let asteroid;
+const MAX_ASTEROIDS = 10;
+let asteroidArray = [];
 
 function preload() {
   asteroid = loadModel("asteroid.obj");
 }
 
+class Asteroid {
+  constuctor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.rotYSpeed = frameCount*0.01;
+    this.meteor = asteroid;
+  }
+
+  display() {
+    for(let asteroid of asteroidArray) {
+      model(asteroid);
+    }
+  }
+
+  rotate() {
+    rotateY(this.rotYSpeed);
+  }
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
+  angleMode(DEGREES);
 }
 
 function draw() {
   background(220);
   orbitControl();
-  push();
-  fill(150);
-  scale(0.1);
-  model(asteroid);
-  pop();
+  for (let i = 0; i<MAX_ASTEROIDS; i++) {
+    let someSpaceRock = new Asteroid;
+    asteroidArray.push(someSpaceRock);
+  }
 }
