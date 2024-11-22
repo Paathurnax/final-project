1442750f8b6d74a21284afe716f615bf0bf42171
Jacob <165198998+Paathurnax@users.noreply.github.com>
2024-11-22 14:16:16 -0600

    tried to make some coordinates static and created some form of a camera perspective

--- sketch.js
+++ sketch.js
@@ -34,17 +34,31 @@ class Asteroid {
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
   angleMode(DEGREES);
-  for (let i = 0; i<MAX_ASTEROIDS; i+=1) {
-    posArray.push(createVector(random(-width/2, width/2), random(-height/2, height/2), random(-width/2, width/2)));
+  for (let i = 0; i<MAX_ASTEROIDS; i++) {
+    if (i<5) {
+      posArray.push(createVector(-width/4, random(-height/2, height/2), random(-width*0.75, width/2)));
+    }
+    else {
+      posArray.push(createVector(width, random(-height/2, height/2), random(-width*0.75, width/2)));
+    }
   }
+  // makePerspective();
 }
 
 function draw() {
   background(220);
   orbitControl();
+  push();
   scale(0.5);
-  for (let i = 0; i<MAX_ASTEROIDS; i+=1) {
+  for (let i = 0; i<MAX_ASTEROIDS; i++) {
     translate(posArray[i].x, posArray[i].y, posArray[i].z);
     model(meteor);
   }
+  pop();
+}
+
+function makePerspective() {
+  let cam = createCamera();
+  cam.setPosition(width/2, height*0.75, 0);
+  cam.lookAt(0, height/2, 0);
 }
