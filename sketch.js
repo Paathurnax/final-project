dcec155a5044fd94594f76b92313e5af236497a0
Jacob <165198998+Paathurnax@users.noreply.github.com>
2024-11-28 14:14:14 -0600

    changed from classes to functions in order to make my code actually work properly

--- sketch.js
+++ sketch.js
@@ -5,48 +5,43 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-const MAX_ASTEROIDS = 10;
+const MAX_ASTEROIDS = 9;
 const ASTEROID_SIZE = 50;
-const MOVE_SPEED = 5;
 let asteroidArray = [];
-let posArray = [];
 let spaceCoalition;
-let space;
-let delay = 50;
+let asteroidDist;
+let moveDelay = 50;
+let creationDelay = 1000;
+let x;
+let isDead = false;
 
-// function preload() {
-//   space = loadImage("space.gif");
-// }
-
-class AsteroidCreation {
-  constuctor(y, distance) {
-    this.speed = MOVE_SPEED;
-    this.asteroidDist = distance;
-    this.y = y;
-  }
-
-  display() {
-    for (let i = 0; i<MAX_ASTEROIDS; i++) {
-      circle(ASTEROID_SIZE, this.y, ASTEROID_SIZE);
-      this.y -= this.asteroidDist;
-    }
-  }
-  // move() {
-  //   if (frameCount%delay === 0) {
-  //     for (let i = 0; i<MAX_ASTEROIDS; i++) {
-  //       posArray[i]
-  //     }
-  //   }
-  // }
-}
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
   angleMode(DEGREES);
-  spaceCoalition = new AsteroidCreation(height, height/MAX_ASTEROIDS);
+  asteroidDist = height/ MAX_ASTEROIDS;
+  x = -width/2;
 }
 
 function draw() {
   background(220);
-  spaceCoalition.display();
+  createAsteroids();
+  moveAsteroids();
+  if (frameCount%creationDelay === 0 || x===width) {
+    createAsteroids();
+  }
+}
+
+function createAsteroids() {
+  let y = height/2;
+  for (let i = 0; i<MAX_ASTEROIDS; i++) {
+    circle(x, y, ASTEROID_SIZE/2);
+    y -= asteroidDist;
+  }
+}
+
+function moveAsteroids() {
+  if (x<width && frameCount%moveDelay === 0) {
+    x += random(10, 50);
+  }
 }
