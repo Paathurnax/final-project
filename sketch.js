f46e014921b122d7ea1810a73de74f91583ab3ae
Jacob <165198998+Paathurnax@users.noreply.github.com>
2024-12-05 12:12:50 -0600

    smoothed out my draw loop and shortened code a bit

--- sketch.js
+++ sketch.js
@@ -5,100 +5,12 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-// const MAX_ASTEROIDS = 10;
-// const ASTEROID_SIZE = 50;
-// let asteroidArray = [];
-// let spaceCoalition;
-// let asteroidDist;
-// let moveDelay = 50;
-// let creationDelay = 1000;
-// let x;
-// let y;
-// let clickCount = 0;
-// let isDead = false;
-
-// // class Create {
-// //   constructor(x, y) {
-// //     this.x = x;
-// //     this.y = y;
-// //     this.isDead = false;
-// //   }
-
-// //   createAsteroid() {
-// //     if (!this.isDead) {
-// //       circle(this.x, this.y, ASTEROID_SIZE/2);
-// //     }
-// //   }
-
-// //   moveAsteroid() {
-// //     if (this.x<width && frameCount%moveDelay === 0) {
-// //       this.x += random(10, 50);
-// //     }
-// //   }
-
-// //   dead() {
-// //     if (clickCount === 3) {
-// //       this.isDead = true;
-// //     }
-// //     else {
-// //       this.isDead = false;
-// //     }
-// //   }
-// // }
-
-// function setup() {
-//   createCanvas(windowWidth, windowHeight, WEBGL);
-//   angleMode(DEGREES);
-//   asteroidDist = height/MAX_ASTEROIDS;
-// }
-
-// function draw() {
-//   background(220);
-//   createAsteroids();
-//   moveAsteroids();
-//   dead();
-// }
-
-// function mousePressed() {
-//   if (dist(mouseX, mouseY, x, y) <= ASTEROID_SIZE) {
-//     clickCount++;
-//   }
-// }
-
-// function createAsteroids() {
-//   let offsetX = 0;
-//   let offsetY = 0;
-//   offsetX += random(ASTEROID_SIZE, ASTEROID_SIZE*2);
-//   offsetY += random(ASTEROID_SIZE, ASTEROID_SIZE*2);
-//   for (let i = 0; i<MAX_ASTEROIDS; i++) {
-//     let y = 100*noise(0.010*frameCount + 10000);
-//     let x = 100*noise(0.010*frameCount);
-//     circle(x+offsetX, y+offsetY, ASTEROID_SIZE/2);
-//   }
-// }
-
-// function moveAsteroids() {
-//   if (x<width && frameCount%moveDelay === 0) {
-//     x += random(10, 50);
-//   }  
-// } 
- 
-
-// function dead() {
-//   if (clickCount === 3) {
-//     isDead = true;
-//   }
-//   else {
-//     isDead = false;
-//   }
-// }
-
 const ROT_ANGLE = 0.1;
 const FORCE_DIVIDER = 0.5;
-const MAX_ASTEROIDS = 10;
+const MAX_ASTEROIDS = 100;
 const VEL_MULTI = 2.5;
 const DRAG = 0.99;
-const MIN_SIZE = 10;
+const MIN_SIZE = 5;
 let ship;
 let asteroids = [];
 let lasers = [];
@@ -114,35 +26,10 @@ function setup() {
 function draw() {
   background(220);
   
-  for (let i = 0; i<asteroids.length; i++) {
-    if (ship.hits(asteroids[i])) {
-      console.log("oops");
-    }
-    asteroids[i].render();
-    asteroids[i].update();
-    asteroids[i].edges();
-  }
   
-  for (let i = lasers.length-1; i>=0; i--) {
-    lasers[i].render();
-    lasers[i].update();
-    if(lasers[i].offScreen()) {
-      lasers.splice(i, 1);
-    }
-    else{     
-      for (let j = asteroids.length-1; j>=0; j--) {
-        if (lasers[i].hits(asteroids[j])) {
-          if (asteroids[j].size > MIN_SIZE) {
-            let newAsteroids = asteroids[j].breakApart();       
-            asteroids = asteroids.concat(newAsteroids);
-          }
-          asteroids.splice(j, 1);
-          lasers.splice(i, 1);
-          break;
-        }
-      }
-    }
-  }
+  
+  laserStuff();
+  asteroidStuff();
   shipFunctions();
 }
 
@@ -173,6 +60,40 @@ function shipFunctions() {
   ship.turn();
   ship.update();
   ship.edges();
+}
+
+function laserStuff() {
+  for (let i = lasers.length-1; i>=0; i--) {
+    lasers[i].render();
+    lasers[i].update();
+    if(lasers[i].offScreen()) {
+      lasers.splice(i, 1);
+    }
+    else{     
+      for (let j = asteroids.length-1; j>=0; j--) {
+        if (lasers[i].hits(asteroids[j])) {
+          if (asteroids[j].size > MIN_SIZE) {
+            let newAsteroids = asteroids[j].breakApart();       
+            asteroids = asteroids.concat(newAsteroids);
+          }
+          asteroids.splice(j, 1);
+          lasers.splice(i, 1);
+          break;
+        }
+      }
+    }
+  }
+}
+
+function asteroidStuff() {
+  for (let i = 0; i<asteroids.length; i++) {
+    if (ship.hits(asteroids[i])) {
+      console.log("oops");
+    }
+    asteroids[i].render();
+    asteroids[i].update();
+    asteroids[i].edges();
+  }
 }
 
 class Ship {
